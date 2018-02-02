cda9669581492d34aff13544d394f68b951c5620
Daniel Sanchez Ceinos <dsanchezc@vectoritcgroup.com>
2018-02-02 19:30:16 +0100

    Change to animator

--- animatedtextview/src/main/java/com/fireshield/animatedtextview/FSAnimatedTV.kt
+++ animatedtextview/src/main/java/com/fireshield/animatedtextview/FSAnimatedTV.kt
@@ -1,10 +1,11 @@
 package com.fireshield.animatedtextview
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.Interpolator
 import android.widget.FrameLayout
 import android.widget.TextView
 
@@ -15,6 +16,8 @@ import android.widget.TextView
 class FSAnimatedTV(context: Context?, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
   var value: Int = 0
+  var outAnim: Animation
+  var inAnim: Animation
 
   fun initialize(context: Context) {
     inflate(context, R.layout.animated_tv, this)
@@ -22,38 +25,35 @@ class FSAnimatedTV(context: Context?, attrs: AttributeSet?) : FrameLayout(contex
 
   init {
     initialize(context!!)
+    outAnim = AnimationUtils.loadAnimation(this.context, R.anim.trans_out)
+    inAnim = AnimationUtils.loadAnimation(this.context, R.anim.trans_in)
   }
 
   fun setNum(num: Int) {
-    val yTrans: Float
-    if (num < value)
-      yTrans = findViewById<View>(R.id.fs_number).height.toFloat()
-    else
-      yTrans = (-findViewById<View>(R.id.fs_number).height).toFloat()
+    outAnim.cancel()
+    inAnim.cancel()
+
+    if (num > value) {
+      outAnim = AnimationUtils.loadAnimation(this.context, R.anim.trans_out)
+      inAnim = AnimationUtils.loadAnimation(this.context, R.anim.trans_in)
+    } else {
+      outAnim = AnimationUtils.loadAnimation(this.context, R.anim.trans_out_inv)
+      inAnim = AnimationUtils.loadAnimation(this.context, R.anim.trans_in_inv)
+    }
+
+    outAnim.interpolator = AccelerateDecelerateInterpolator()
+    inAnim.interpolator = AccelerateDecelerateInterpolator()
 
     value = num
     val text = findViewById<TextView>(R.id.fs_number)
-
-    text.animate()
-        .translationY(yTrans)
-        .alpha(0F)
-        .setDuration(200)
-        .setListener(object : AnimatorListenerAdapter() {
-          override fun onAnimationEnd(animation: Animator) {
-            super.onAnimationEnd(animation)
-            text.text = num.toString()
-            text.y = -yTrans
-            text.animate()
-                .translationY(0F)
-                .alpha(1F)
-                .setDuration(200)
-                .setListener(object : AnimatorListenerAdapter() {
-                  override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                    text.visibility = View.VISIBLE
-                  }
-                })
-          }
-        })
+    outAnim.setAnimationListener(object : Animation.AnimationListener {
+      override fun onAnimationStart(arg0: Animation) {}
+      override fun onAnimationRepeat(arg0: Animation) {}
+      override fun onAnimationEnd(arg0: Animation) {
+        text.text = num.toString()
+        text.startAnimation(inAnim)
+      }
+    })
+    text.startAnimation(outAnim)
   }
 }
